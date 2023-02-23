5617cf079164b5f1f0f8181aa42d67a7a468174b
MathiasDigit <bellmathias33@gmail.com>
2023-02-23 14:24:01 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 
 
 /**
- * main - prints with proper grammar, but the outcome 
- * is a piece of art, followed  new line
+ * main - prints "with proper grammar, but the outcome 
+ * is a piece of art," followed newline
  * Return: Always 0.
  *
  */
