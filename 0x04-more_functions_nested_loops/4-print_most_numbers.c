8dc1990fa037ff47a7a515b903e5ca36e30f8411
MathiasDigit <bellmathias33@gmail.com>
2023-02-24 09:16:05 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,19 +13,14 @@ void print_most_numbers(void)
 
 	for(; n <= 9; n++)
 	{
-		if (n == 2 || n == 4)
-		{
-
-			continue;
-
-		}
-
-		else
-		{
-
-			_putchar(n + '0');
-
-		}
+	if (n == 2 || n == 4)
+	{
+        continue;
+	}
+	else
+	{
+        _putchar(n + '0');
+        }
 	}
 	_putchar('\n');
 }
